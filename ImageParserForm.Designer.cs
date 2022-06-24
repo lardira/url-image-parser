ceec59bb72e48599d89255714181ebcdf3e16bde
lardira <danilkefimov@gmail.com>
2022-06-24 23:05:06 +0300

    init with app frontend

---
+++ ImageParserForm.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace ImageParser
+{
+    partial class ImageParserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImageParserForm));
+            this.settingsGroupBox = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.domainUpDown3 = new System.Windows.Forms.DomainUpDown();
+            this.domainUpDown4 = new System.Windows.Forms.DomainUpDown();
+            this.filtersLabel = new System.Windows.Forms.Label();
+            this.domainUpDown2 = new System.Windows.Forms.DomainUpDown();
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.URLLabel = new System.Windows.Forms.Label();
+            this.URLtextBox = new System.Windows.Forms.TextBox();
+            this.savePathButton = new System.Windows.Forms.Button();
+            this.savePathLabel = new System.Windows.Forms.Label();
+            this.savePathTextBox = new System.Windows.Forms.TextBox();
+            this.imageFoundGroupBox = new System.Windows.Forms.GroupBox();
+            this.imageFound = new System.Windows.Forms.PictureBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.startButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.settingsGroupBox.SuspendLayout();
+            this.imageFoundGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageFound)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // settingsGroupBox
+            // 
+            this.settingsGroupBox.Controls.Add(this.label6);
+            this.settingsGroupBox.Controls.Add(this.label5);
+            this.settingsGroupBox.Controls.Add(this.label3);
+            this.settingsGroupBox.Controls.Add(this.label4);
+            this.settingsGroupBox.Controls.Add(this.label2);
+            this.settingsGroupBox.Controls.Add(this.label1);
+            this.settingsGroupBox.Controls.Add(this.domainUpDown3);
+            this.settingsGroupBox.Controls.Add(this.domainUpDown4);
+            this.settingsGroupBox.Controls.Add(this.filtersLabel);
+            this.settingsGroupBox.Controls.Add(this.domainUpDown2);
+            this.settingsGroupBox.Controls.Add(this.domainUpDown1);
+            this.settingsGroupBox.Controls.Add(this.URLLabel);
+            this.settingsGroupBox.Controls.Add(this.URLtextBox);
+            this.settingsGroupBox.Controls.Add(this.savePathButton);
+            this.settingsGroupBox.Controls.Add(this.savePathLabel);
+            this.settingsGroupBox.Controls.Add(this.savePathTextBox);
+            this.settingsGroupBox.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.settingsGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.settingsGroupBox.Name = "settingsGroupBox";
+            this.settingsGroupBox.Size = new System.Drawing.Size(310, 255);
+            this.settingsGroupBox.TabIndex = 0;
+            this.settingsGroupBox.TabStop = false;
+            this.settingsGroupBox.Text = "Settings";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(220, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 21);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "y:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(220, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 21);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "y:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(126, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 21);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "x:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(126, 188);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 21);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "x:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // domainUpDown3
+            // 
+            this.domainUpDown3.Font = new System.Drawing.Font("Yu Gothic UI", 10F);
+            this.domainUpDown3.Location = new System.Drawing.Point(154, 188);
+            this.domainUpDown3.Name = "domainUpDown3";
+            this.domainUpDown3.Size = new System.Drawing.Size(56, 25);
+            this.domainUpDown3.TabIndex = 8;
+            this.domainUpDown3.Text = "0";
+            this.domainUpDown3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // domainUpDown4
+            // 
+            this.domainUpDown4.Font = new System.Drawing.Font("Yu Gothic UI", 10F);
+            this.domainUpDown4.Location = new System.Drawing.Point(154, 216);
+            this.domainUpDown4.Name = "domainUpDown4";
+            this.domainUpDown4.Size = new System.Drawing.Size(56, 25);
+            this.domainUpDown4.TabIndex = 9;
+            this.domainUpDown4.Text = "0";
+            this.domainUpDown4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // filtersLabel
+            // 
+            this.filtersLabel.AutoSize = true;
+            this.filtersLabel.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold);
+            this.filtersLabel.Location = new System.Drawing.Point(6, 162);
+            this.filtersLabel.Name = "filtersLabel";
+            this.filtersLabel.Size = new System.Drawing.Size(54, 21);
+            this.filtersLabel.TabIndex = 7;
+            this.filtersLabel.Text = "Filters";
+            this.filtersLabel.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // domainUpDown2
+            // 
+            this.domainUpDown2.Font = new System.Drawing.Font("Yu Gothic UI", 10F);
+            this.domainUpDown2.Location = new System.Drawing.Point(248, 188);
+            this.domainUpDown2.Name = "domainUpDown2";
+            this.domainUpDown2.Size = new System.Drawing.Size(56, 25);
+            this.domainUpDown2.TabIndex = 6;
+            this.domainUpDown2.Text = "0";
+            this.domainUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.Font = new System.Drawing.Font("Yu Gothic UI", 10F);
+            this.domainUpDown1.Location = new System.Drawing.Point(248, 216);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.Size = new System.Drawing.Size(56, 25);
+            this.domainUpDown1.TabIndex = 6;
+            this.domainUpDown1.Text = "0";
+            this.domainUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // URLLabel
+            // 
+            this.URLLabel.AutoSize = true;
+            this.URLLabel.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold);
+            this.URLLabel.Location = new System.Drawing.Point(6, 40);
+            this.URLLabel.Name = "URLLabel";
+            this.URLLabel.Size = new System.Drawing.Size(39, 21);
+            this.URLLabel.TabIndex = 4;
+            this.URLLabel.Text = "URL";
+            this.URLLabel.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // URLtextBox
+            // 
+            this.URLtextBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.URLtextBox.Font = new System.Drawing.Font("Yu Gothic UI", 12F);
+            this.URLtextBox.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.URLtextBox.Location = new System.Drawing.Point(6, 64);
+            this.URLtextBox.Name = "URLtextBox";
+            this.URLtextBox.Size = new System.Drawing.Size(298, 29);
+            this.URLtextBox.TabIndex = 3;
+            this.URLtextBox.Text = " type url here...";
+            // 
+            // savePathButton
+            // 
+            this.savePathButton.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.savePathButton.Font = new System.Drawing.Font("Yu Gothic UI", 12F);
+            this.savePathButton.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.savePathButton.Location = new System.Drawing.Point(275, 124);
+            this.savePathButton.Name = "savePathButton";
+            this.savePathButton.Size = new System.Drawing.Size(29, 29);
+            this.savePathButton.TabIndex = 2;
+            this.savePathButton.Text = "...";
+            this.savePathButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.savePathButton.UseVisualStyleBackColor = false;
+            // 
+            // savePathLabel
+            // 
+            this.savePathLabel.AutoSize = true;
+            this.savePathLabel.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold);
+            this.savePathLabel.Location = new System.Drawing.Point(6, 100);
+            this.savePathLabel.Name = "savePathLabel";
+            this.savePathLabel.Size = new System.Drawing.Size(80, 21);
+            this.savePathLabel.TabIndex = 1;
+            this.savePathLabel.Text = "Save Path";
+            this.savePathLabel.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // savePathTextBox
+            // 
+            this.savePathTextBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.savePathTextBox.Font = new System.Drawing.Font("Yu Gothic UI", 12F);
+            this.savePathTextBox.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.savePathTextBox.Location = new System.Drawing.Point(6, 124);
+            this.savePathTextBox.Name = "savePathTextBox";
+            this.savePathTextBox.Size = new System.Drawing.Size(262, 29);
+            this.savePathTextBox.TabIndex = 0;
+            this.savePathTextBox.Text = " choose the destination folder...";
+            // 
+            // imageFoundGroupBox
+            // 
+            this.imageFoundGroupBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.imageFoundGroupBox.Controls.Add(this.imageFound);
+            this.imageFoundGroupBox.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.imageFoundGroupBox.Location = new System.Drawing.Point(12, 274);
+            this.imageFoundGroupBox.Name = "imageFoundGroupBox";
+            this.imageFoundGroupBox.Size = new System.Drawing.Size(310, 202);
+            this.imageFoundGroupBox.TabIndex = 5;
+            this.imageFoundGroupBox.TabStop = false;
+            this.imageFoundGroupBox.Text = "Image Found";
+            // 
+            // imageFound
+            // 
+            this.imageFound.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.imageFound.Image = ((System.Drawing.Image)(resources.GetObject("imageFound.Image")));
+            this.imageFound.InitialImage = ((System.Drawing.Image)(resources.GetObject("imageFound.InitialImage")));
+            this.imageFound.Location = new System.Drawing.Point(6, 32);
+            this.imageFound.Name = "imageFound";
+            this.imageFound.Size = new System.Drawing.Size(298, 164);
+            this.imageFound.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.imageFound.TabIndex = 0;
+            this.imageFound.TabStop = false;
+            // 
+            // progressBar
+            // 
+            this.progressBar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.progressBar.Location = new System.Drawing.Point(-6, 533);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(347, 15);
+            this.progressBar.TabIndex = 1;
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.startButton.FlatAppearance.BorderColor = System.Drawing.SystemColors.WindowFrame;
+            this.startButton.FlatAppearance.BorderSize = 0;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(-6, 481);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(347, 50);
+            this.startButton.TabIndex = 2;
+            this.startButton.TabStop = false;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Yu Gothic UI", 12F);
+            this.label5.Location = new System.Drawing.Point(6, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 21);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Max Image Size";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Yu Gothic UI", 12F);
+            this.label6.Location = new System.Drawing.Point(6, 215);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(116, 21);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Min Image Size";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // ImageParserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(334, 544);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.imageFoundGroupBox);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.settingsGroupBox);
+            this.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
+            this.Name = "ImageParserForm";
+            this.Text = "ImageParser";
+            this.settingsGroupBox.ResumeLayout(false);
+            this.settingsGroupBox.PerformLayout();
+            this.imageFoundGroupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imageFound)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox settingsGroupBox;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label URLLabel;
+        private System.Windows.Forms.TextBox URLtextBox;
+        private System.Windows.Forms.Button savePathButton;
+        private System.Windows.Forms.Label savePathLabel;
+        private System.Windows.Forms.TextBox savePathTextBox;
+        private System.Windows.Forms.GroupBox imageFoundGroupBox;
+        private System.Windows.Forms.PictureBox imageFound;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private System.Windows.Forms.DomainUpDown domainUpDown3;
+        private System.Windows.Forms.DomainUpDown domainUpDown4;
+        private System.Windows.Forms.Label filtersLabel;
+        private System.Windows.Forms.DomainUpDown domainUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
