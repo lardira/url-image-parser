04bf63cb7ea000daf583e894a7af6d0de8e3f43a
lardira <danilkefimov@gmail.com>
2022-06-24 23:48:43 +0300

    added util class for storing Sizes

---
+++ Size.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageParser
+{
+    internal struct Size
+    {
+        public uint x, y;
+
+        public Size(uint x = 0, uint y = 0)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
